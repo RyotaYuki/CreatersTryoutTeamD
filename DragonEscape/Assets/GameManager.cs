c772f4c49d3273e99d44e094703f023164864384
SekineMizuki <mitakin29@gmail.com>
2019-12-18 11:11:30 +0900

    GameManagerスクリプトを作成 PlayerスクリプトをGameManager参照するように調整

---
+++ DragonEscape/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //
+    private float _gamespeed { get; set; }//ゲームの進行スピード
+    private float _elapsedtime { get; set; }//ゲームが始まってから経った時間
+    private int _gamemode { get; set; }//現在のゲームモード 0がタイトル　1がプレイ中　2がリザルト
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetGameMode()
+    {
+        return _gamemode;
+    }
+}
--- DragonEscape/Assets/Scripts/Player.cs
+++ DragonEscape/Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    //ゲームマネージャー
+    private GameManager _gm;
+
     //ステータス
     public int hp = 100;
     public int maxhp = 100;
@@ -26,13 +29,10 @@ public class Player : MonoBehaviour
 
     //速度処理用
     private Vector3 _force;
-
     [SerializeField]
     private float forwordSpeed = 10; //速度係数
     [SerializeField]
     private float rotateSpeed = 5;//回転係数
-
-
     private Rigidbody _rb;
     private Vector3 movevector;
     public float moveForceMultiplier; // 移動速度の入力に対する追従度
@@ -46,40 +46,38 @@ public class Player : MonoBehaviour
     {
         _force = transform.position;
         _rb = GetComponent<Rigidbody>();
+        _gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-
-        if (Input.GetKey("w"))
+        //ゲームマネージャーから現在のゲームの状態を取得
+        int gamemode = 0;
+        if (_gm)
         {
-            if(maxspeed > _speed)
-            {
-                _speed += 10.0f * Time.deltaTime;
-            }
+            gamemode = _gm.GetGameMode();
         }
 
-        else if(minspeed < _speed)
+        if (gamemode == 1)
         {
-            _speed -= 30.0f * Time.deltaTime;
-        }
+            //移動処理
+            Move();
+            //スピード調整
+            SpeedControl();
+            //UI変更処理
+            UIUpdate();
 
-        if (Input.GetKeyDown("f"))
-        {
-            Instantiate(moneyObj, transform.position,Quaternion.identity);
-            _money -= 10000;
         }
+        
 
-        //UI変更処理
-        hpbar.fillAmount = (float)hp/(float)maxhp;
-        moneyText.text = _money + "＄";
 
     }
 
     void Move()
     {
+
+
 
         _moveY = Input.GetAxis("Vertical") * forwordSpeed * _speed * Time.deltaTime;
         _force = transform.up * _moveY * 10;
@@ -101,6 +99,41 @@ public class Player : MonoBehaviour
         {
             _rb.AddForce(moveForceMultiplier * (new Vector3(movevector.x, 0, movevector.z) - _rb.velocity));
             transform.Rotate(0, 0, -Z_Rotation);
+        }
+    }
+    //ＵＩ更新
+    void UIUpdate()
+    {
+        //UI変更処理
+        hpbar.fillAmount = (float)hp / (float)maxhp;
+        moneyText.text = _money + "＄";
+    }
+
+    //スピード更新
+    void SpeedControl()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            if (maxspeed > _speed)
+            {
+                _speed += 10.0f * Time.deltaTime;
+            }
+        }
+
+        else if (minspeed < _speed)
+        {
+            _speed -= 30.0f * Time.deltaTime;
+        }
+    }
+
+    //物を投げる処理
+    void ItemThrow()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            Instantiate(moneyObj, transform.position, Quaternion.identity);
+            _money -= 10000;
         }
     }
 
