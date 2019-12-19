cb921012f361f414aaf135fe122fda2b21c34f4d
SekineMizuki <mitakin29@gmail.com>
2019-12-19 10:12:56 +0900

    プレイヤーの回転調整とコード整理　UI制御用のクラスを作成

---
+++ DragonEscape/Assets/PlayerUIInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIInfo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _UIs;//
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void UIFeedIn()
+    {
+        foreach (GameObject ui in _UIs)
+        {
+            Color c = Color.black;
+            if (ui.GetComponent<Image>())
+            {
+                Image image = ui.GetComponent<Image>();
+                c = image.color;
+                image.color = new Color(c.r, c.g, c.b, c.a + 0.1f);
+            }
+            if (ui.GetComponent<Text>())
+            {
+                Text text = ui.GetComponent<Text>();
+                c = text.color;
+                text.color = new Color(c.r, c.g, c.b, c.a + 0.1f);
+            }
+        }
+    }
+
+    void UIFeedOut()
+    {
+
+    }
+}
